25eb8e7f5ab5e22d5a82cb8e2f1d2a7872fdfa91
Isabella <isabellarl573@gmail.com>
2021-10-25 07:30:34 -0400

    validator

--- CC/Shared/Validator.cs
+++ CC/Shared/Validator.cs
@@ -7,11 +7,12 @@ namespace CC.Shared
 {
     public class Validator
     {
-        public bool isMoveLegal(Piece piece, Piece[,] pieceBoard, string fen, int startRow, int StartColumn, int endRow, int endStartColumn) 
+        public bool isMoveLegal(Piece piece, Piece[,] pieceBoard, string fen, int startRow, int startColumn, int endRow, int endColumn) 
         {
             switch(piece.Type)
             {
                 case PieceType.Bishop:
+                    if
                     break;
                 case PieceType.Knight:
                     break;
@@ -36,5 +37,60 @@ namespace CC.Shared
         {
             return false;
         }
+
+
+        private bool isValidBishopMove(int startRow, int startColumn, int endRow, int endColumn)
+        {
+            if (Math.Abs(startRow - endRow) == Math.Abs(startColumn - endColumn))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool isValidKnightMove(int startRow, int startColumn, int endRow, int endColumn)
+        {
+            if ((Math.Abs(startRow - endRow) == 1 && Math.Abs(startColumn - endColumn) == 2) || (Math.Abs(startRow - endRow) == 2 && Math.Abs(startColumn - endColumn) == 1))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool isValidRookMove(int startRow, int startColumn, int endRow, int endColumn)
+        {
+            if (Math.Abs(startRow - endRow) == 0 || Math.Abs(startColumn - endColumn) == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        private bool isValidPawnMove(int startRow, int startColumn, int endRow, int endColumn)
+        {
+            //first move - 2 steps
+            if (Math.Abs(startRow - endRow) == 1 && Math.Abs(startColumn - endColumn) == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        private bool isValidPawnEatingMove(int startRow, int startColumn, int endRow, int endColumn)
+        {
+            if (Math.Abs(startRow - endRow) == 1 && Math.Abs(startColumn - endColumn) == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool isValidKingMove(int startRow, int startColumn, int endRow, int endColumn)
+        {
+            if (Math.Abs(startRow - endRow) == 1 && Math.Abs(startColumn - endColumn) == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
     }
 }
