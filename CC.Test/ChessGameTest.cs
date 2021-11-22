a60775c5e842d1360be4c79da6a14c95b0738277
Isabella <isabellarl573@gmail.com>
2021-11-22 14:59:10 -0500

    Validator tests

--- CC.Test/ChessGameTest.cs
+++ CC.Test/ChessGameTest.cs
@@ -12,7 +12,37 @@ namespace CC.Test
     public class ChessGameTest
     {
         static ChessGame game = new ChessGame();
-        
+
+        public bool DoubleArrayEquals(String[,] arr1, String[,] arr2)
+        {
+            for (int i = 0; i < arr1.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr1.GetLength(1); j++)
+                {
+                    if (arr1[i, j] != arr2[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        [Fact]
+        public void GenerateBoard()
+        {
+            String fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -0 1";
+
+            string[,] board = new string[,] { {"r","n", "b", "q", "k", "b", "n", "r"},
+                                              {"p","p", "p", "p", "p", "p", "p", "p"},
+                                              {"em", "em", "em", "em", "em", "em", "em", "em"},
+                                              {"em", "em", "em", "em", "em", "em", "em", "em"},
+                                              {"em", "em", "em", "em", "em", "em", "em", "em"},
+                                              {"em", "em", "em", "em", "em", "em", "em", "em"},
+                                              {"P","P", "P", "P", "P", "P", "P", "P"},
+                                              {"R","N", "B", "Q", "K", "B", "N", "R"} };
+            Assert.True(DoubleArrayEquals(board, game.generateFen(fen)));
+        }
 
         [Fact]
         public void GenerateFEN()
--- CC.Test/ValidatorTest.cs
+++ CC.Test/ValidatorTest.cs
@@ -13,211 +13,153 @@ namespace CC.Test
 
         string fen;
 
-        Piece[,] pieceBoard;
+        bool[,] outputBoard;
 
         string[,] board;
 
-        [Fact]
-        public void GenerateBoard()
+        public bool DoubleArrayEquals(bool[,] arr1, bool[,] arr2)
         {
-            fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -0 1";
-            pieceBoard = game.generatePieceBoard(fen);
-            board = game.generateBoard(fen);
-            Assert.True(board[0,0] =="br" && (pieceBoard[0,0].Type == PieceType.Rook && pieceBoard[0, 0].Color == PieceColor.Black));
+            for (int i = 0; i < arr1.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr1.GetLength(1); j++)
+                {
+                    if (arr1[i, j] != arr2[i, j])
+                    {
+                       return false;
+                    }
+                }
+            }
+            return true;
         }
-
 
 
         [Fact]
         public void ValidateMove()
         {
             fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -0 1";
-            pieceBoard = game.generatePieceBoard(fen);
             board = game.generateBoard(fen);
-            Assert.True(validator.isMoveLegal(pieceBoard[6,2], pieceBoard, fen, 6, 2, 5, 2));
+            outputBoard = new bool[,] { {false, false, false, false, false, false, false, false },
+                                        {false, false, false, false, false, false, false, false },
+                                    {false, false, false, false, false, false, false, false },
+                                    {false, false, false, false, false, false, false, false },
+                                    {false, false, false, false, false, false, false, false },
+                                    {false, false, false, false, false, false, false, false },
+                                        {false, false, false, false, false, false, false, false },
+                                        {false, false, false, false, false, false, false, false }};
+            Assert.True(DoubleArrayEquals(outputBoard, Validator.LegalMoves(board, 0, 0, "r")));
         }
 
         [Fact]
         public void ValidateMove2()
         {
             fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -0 1";
-            pieceBoard = game.generatePieceBoard(fen);
             board = game.generateBoard(fen);
-            Assert.True(validator.isMoveLegal(pieceBoard[0, 1], pieceBoard, fen, 0, 1, 2, 2));
-        }
-
-        [Fact]
-        public void InvalidateMove()
-        {
-            fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -0 1";
-            pieceBoard = game.generatePieceBoard(fen);
-            board = game.generateBoard(fen);
-            Assert.False(validator.isMoveLegal(pieceBoard[0, 0], pieceBoard, fen, 0, 0, 0, 7));
-        }
-
-        [Fact]
-        public void InvalidateMove2()
-        {
-            fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -0 1";
-            pieceBoard = game.generatePieceBoard(fen);
-            board = game.generateBoard(fen);
-            Assert.False(validator.isMoveLegal(pieceBoard[0, 1], pieceBoard, fen, 0, 1, 1, 3));
-        }
-
-        [Fact]
-        public void InvalidateMove3()
-        {
-            fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -0 1";
-            pieceBoard = game.generatePieceBoard(fen);
-            board = game.generateBoard(fen);
-            Assert.False(validator.isMoveLegal(pieceBoard[0, 5], pieceBoard, fen, 0, 5, 1, 3));
+            outputBoard = new bool[,] { {false, false, false, false, false, false, false, false },
+                                        {false, false, false, false, false, false, false, false },
+                                    {true, false, false, false, false, false, false, false },
+                                    {true, false, false, false, false, false, false, false },
+                                    {false, false, false, false, false, false, false, false },
+                                    {false, false, false, false, false, false, false, false },
+                                        {false, false, false, false, false, false, false, false },
+                                        {false, false, false, false, false, false, false, false }};
+            Assert.True(DoubleArrayEquals(outputBoard, Validator.LegalMoves(board, 0, 0, "p")));
         }
 
         [Fact]
         public void ValidateBishop1()
         {
-            
-            Assert.True(validator.isValidBishopMove(7, 2, 6, 3));
+            fen = "rnbqkbnr/pppppppp/8/3b4/8/8/PPPPPPPP/RNBQKBNR w KQkq -0 1";
+            board = game.generateBoard(fen);
+            outputBoard = new bool[,] { {false, false, false, false, false, false, false, false },
+                                        {false, false, false, false, false, false, false, false },
+                                    {false, false, true, false, true, false, false, false },
+                                    {false, false, false, false, false, false, false, false },
+                                    {false, false, true, false, true, false, false, false },
+                                    {false, true, false, false, false, true, false, false },
+                                        {true, false, false, false, false, false, true, false },
+                                        {false, false, false, false, false, false, false, false }};
+            Assert.True(DoubleArrayEquals(outputBoard, Validator.LegalBishopMoves(board, 3, 3, "b")));
         }
 
-        [Fact]
-        public void ValidateBishop2()
-        {
-            
-            Assert.True(validator.isValidBishopMove(5, 5, 6, 4));
-        }
-
-        [Fact]
-        public void ValidateBishop3()
-        {
-            
-            Assert.True(validator.isValidBishopMove(5, 5, 4, 6));
-        }
-
-        [Fact]
-        public void ValidateBishop4()
-        {
-            
-            Assert.True(validator.isValidBishopMove(5, 5, 4, 4));
-        }
-
-        [Fact]
-        public void ValidateBishop5()
-        {
-            
-            Assert.True(validator.isValidBishopMove(5, 5, 3, 3));
-        }
-
-        [Fact]
-        public void InvalidateBishop1()
-        {
-            
-            Assert.False(validator.isValidBishopMove(5, 5, 5, 4));
-        }
-
-        [Fact]
-        public void InvalidateBishop2()
-        {
-            
-            Assert.False(validator.isValidBishopMove(5, 5, 7, 4));
-        }
 
         [Fact]
         public void ValidateKnight1()
         {
-            
-            Assert.True(validator.isValidKnightMove(7, 2, 6, 4));
-        }
-
-        [Fact]
-        public void ValidateKnight2()
-        {
-            
-            Assert.True(validator.isValidKnightMove(5, 5, 6, 3));
+            fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -0 1";
+            board = game.generateBoard(fen);
+            outputBoard = new bool[,] { {false, false, false, false, false, false, false, false },
+                                        {false, false, false, false, false, false, false, false },
+                                        {true, false, true, false, false, false, false, false },
+                                        {false, false, false, false, false, false, false, false },
+                                    {false, false, false, false, false, false, false, false },
+                                    {false, false, false, false, false, false, false, false },
+                                        {false, false, false, false, false, false, false, false },
+                                        {false, false, false, false, false, false, false, false }};
+            Assert.True(DoubleArrayEquals(outputBoard, Validator.LegaKnightlMoves(board, 0, 1, "n")));
         }
 
         [Fact]
         public void ValidateRook1()
         {
-            
-            Assert.True(validator.isValidRookMove(7, 2, 7, 4));
-        }
-
-        [Fact]
-        public void ValidateRook2()
-        {
-            
-            Assert.True(validator.isValidRookMove(5, 5, 6, 5));
-        }
-
-        [Fact]
-        public void InvalidateRook1()
-        {
-
-            Assert.False(validator.isValidRookMove(7, 2, 6, 4));
-        }
-
-        [Fact]
-        public void InvalidateRook2()
-        {
-
-            Assert.False(validator.isValidRookMove(5, 5, 6, 3));
+            fen = "rnbqkbnr/1ppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -0 1";
+            board = game.generateBoard(fen);
+            outputBoard = new bool[,] { {false, false, false, false, false, false, false, false },
+                                        {true, false, false, false, false, false, false, false },
+                                        {true, false, false, false, false, false, false, false },
+                                        {true, false, false, false, false, false, false, false },
+                                        {true, false, false, false, false, false, false, false },
+                                        {true, false, false, false, false, false, false, false },
+                                        {true, false, false, false, false, false, false, false },
+                                        {false, false, false, false, false, false, false, false }};
+            Assert.True(DoubleArrayEquals(outputBoard, Validator.LegalRookMoves(board, 0, 0, "r")));
         }
 
         [Fact]
         public void ValidateQueen1()
         {
-            
-            Assert.True(validator.isValidQueenMove(7, 2, 5, 4));
-        }
-
-        [Fact]
-        public void ValidateQueen2()
-        {
-            
-            Assert.True(validator.isValidQueenMove(5, 5, 5, 3));
+            fen = "rnbqkbnr/pppppppp/8/3q4/8/8/PPPPPPPP/RNBQKBNR w KQkq -0 1";
+            board = game.generateBoard(fen);
+            outputBoard = new bool[,] { {false, false, false, false, false, false, false, false },
+                                        {false, false, false, false, false, false, false, false },
+                                        {false, false, true, true, true, false, false, false },
+                                        {true, true, true, x, true, true, true, true },
+                                        {false, false, true, true, true, false, false, false },
+                                        {false, true, false, true, false, true, false, false },
+                                        {true, false, false, true, false, false, true, false },
+                                        {false, false, false, false, false, false, false, false }};
+            Assert.True(DoubleArrayEquals(outputBoard, Validator.LegalQueenMoves(board, 0, 0, "q")));
         }
 
         [Fact]
         public void ValidateKing1()
         {
-            
-            Assert.True(validator.isValidKingMove(7, 2, 6, 3));
-        }
-
-        [Fact]
-        public void ValidateKing2()
-        {
-            
-            Assert.True(validator.isValidKingMove(5, 5, 6, 4));
+            fen = "rnbqkbnr/pppppppp/8/3k4/8/8/PPPPPPPP/RNBQKBNR w KQkq -0 1";
+            board = game.generateBoard(fen);
+            outputBoard = new bool[,] { {false, false, false, false, false, false, false, false },
+                                        {false, false, false, false, false, false, false, false },
+                                    {false, false, true, true, true, false, false, false },
+                                    {false, false, true, false, true, false, false, false },
+                                    {false, false, true, true, true, false, false, false },
+                                    {false, false, false, false, false, false, false, false },
+                                        {false, false, false, false, false, false, false, false },
+                                        {false, false, false, false, false, false, false, false }};
+            Assert.True(DoubleArrayEquals(outputBoard, Validator.LegalKingMoves(board, 3, 3, "k")));
         }
 
         [Fact]
         public void ValidatePawn1()
         {
-            
-            Assert.True(validator.isValidPawnMove(7, 2, 6, 2));
-        }
-
-        [Fact]
-        public void ValidatePawn2()
-        {
-            
-            Assert.True(validator.isValidPawnMove(5, 5, 6, 5));
-        }
-
-        [Fact]
-        public void ValidatePawnEating1()
-        {
-
-            Assert.True(validator.isValidPawnEatingMove(7, 2, 6, 3));
-        }
-
-        [Fact]
-        public void ValidatePawnEating2()
-        {
-
-            Assert.True(validator.isValidPawnEatingMove(5, 5, 6, 4));
+            fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -0 1";
+            board = game.generateBoard(fen);
+            outputBoard = new bool[,] { {false, false, false, false, false, false, false, false },
+                                        {false, false, false, false, false, false, false, false },
+                                    {false, true, false, false, false, false, false, false },
+                                    {false, true, false, false, false, false, false, false },
+                                    {false, false, false, false, false, false, false, false },
+                                    {false, false, false, false, false, false, false, false },
+                                        {false, false, false, false, false, false, false, false },
+                                        {false, false, false, false, false, false, false, false }};
+            Assert.True(DoubleArrayEquals(outputBoard, Validator.LegalPawnMoves(board, 1, 1, "p")));
         }
     }
 }
